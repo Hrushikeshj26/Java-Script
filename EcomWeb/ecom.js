bba4984f7549e9aec68d5a2e7978bb60c4726ac4
Hrushikeshj26 <hrushij92@gmail.com>
2025-04-23 09:54:58 +0530

    added prodimgs and arrenge files

--- EcomWeb/ecom.js
+++ EcomWeb/ecom.js
@@ -2,56 +2,56 @@ const products = [
   {
     id: 1,
     name: "Wireless Headphones",
-    img: "https://via.placeholder.com/300",
+    img: "/Ecom/EcomWeb/prodimg/wireless headphones.jpg",
     desc: "High-quality wireless headphones with noise cancellation.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quis quod neque culpa omnis nobis? Voluptate!",
     price: 59.99
   },
   {
     id: 2,
     name: "Smartwatch",
-    img: "https://via.placeholder.com/300",
+    img: "/Ecom/EcomWeb/prodimg/smartwatch.jpg",
     desc: "Stylish smartwatch with fitness tracking features.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quis quod neque culpa omnis nobis? Voluptate!",
     price: 89.99
   },
   {
     id: 3,
     name: "Bluetooth Speaker",
-    img: "https://via.placeholder.com/300",
+    img: "/Ecom/EcomWeb/prodimg/speaker.jpg",
     desc: "Portable speaker with excellent sound quality.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quis quod neque culpa omnis nobis? Voluptate!",
     price: 39.99
   },
   {
     id: 4,
     name: "Laptop Stand",
-    img: "https://via.placeholder.com/300",
+    img: "/Ecom/EcomWeb/prodimg/laptop stand.png",
     desc: "Ergonomic laptop stand for better posture.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quis quod neque culpa omnis nobis? Voluptate!",
     price: 29.99
   },
   {
     id: 5,
     name: "Gaming Mouse",
-    img: "https://via.placeholder.com/300",
+    img: "/Ecom/EcomWeb/prodimg/gaming mouse.jpg",
     desc: "Ergonomic laptop stand for better posture.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quis quod neque culpa omnis nobis? Voluptate!",
     price: 19.99
   },
   {
     id: 6,
     name: "Portable Charger",
-    img: "https://via.placeholder.com/300",
+    img: "/Ecom/EcomWeb/prodimg/Portable-charger.jpg",
     desc: "High-capacity portable charger with fast charging capabilities.Lorem ipsum dolor sit amet consectetur adipisicing",
     price: 49.99
   },
   {
     id: 7,
     name: "Wireless Mouse",
-    img: "https://via.placeholder.com/300",
+    img: "/Ecom/EcomWeb/prodimg/gaming mouse.jpg",
     desc: "Ergonomic wireless mouse with customizable buttons and RGB lighting.",
     price: 29.99
   },
   {
     id: 8,
     name: "Smartphone",
-    img: "https://via.placeholder.com/300",
+    img: "/Ecom/EcomWeb/prodimg/smartphone.jpg",
     desc: "Latest smartphone with cutting-edge features and sleek design.",
     price: 62.99
   }
@@ -78,9 +78,9 @@ let totalItems = null;
 
 products.forEach(product => {
 const productItem = document.createElement('div');
-productItem.className = "bg-gray-600 shadow rounded p-4 cursor-pointer hover:shadow-lg";
+productItem.className = "bg-gray-800 shadow rounded p-4 w-74 h-94 cursor-pointer hover:shadow-lg";
 productItem.innerHTML = `
-    <img src="${product.img}"class="w-full h-40 object-cover mb-2 rounded">
+    <img src="${product.img}"class="w-full h-60 object-fit mb-2 rounded">
     <h3 class="font-semibold text-white text-lg">${product.name}</h3>
     <p class="text-sm text-white">$${product.price}</p>
   `;
