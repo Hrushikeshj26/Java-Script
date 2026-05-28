1d40fba8e295aca77fc86a66d212b460228eb3ee
Hrushikeshj26 <hrushij92@gmail.com>
2026-05-29 00:08:01 +0530

    JS Practice 07

---
+++ Practice/test07.js
@@ -0,0 +1,33 @@
+// Async Await
+
+
+function fetchUserProfile() {
+  return new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+      resolve({ 
+        name: "Hrushikesh", 
+        role: "Frontend Developer", 
+        location: "Belgaum" 
+      });
+    }, 2000);
+
+  });
+}
+
+async function displayUser() {
+    try {
+
+        let userData = await fetchUserProfile();
+        let {name, role, location} = userData;
+
+        console.log(`Welcome ${name}, ${role} from ${location}.`);
+
+    } catch (error) {
+        
+        console.log('Error fetching data');
+        
+    }
+}
+
+displayUser();
